17d8f44084a2b145d863fecb969bdda1c4f9eede
asmaa mousa <asmaamousa978@gmail.com>
2024-11-09 15:12:40 +0200

    create edit  note view

--- lib/widgets/addNoteButtomSheet.dart
+++ lib/widgets/addNoteButtomSheet.dart
@@ -1,5 +1,10 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:notes/constants.dart';
 import 'package:notes/widgets/CustomTextField.dart';
+import 'package:notes/widgets/customButton.dart';
 
 class AddNotesButtomSheet extends StatelessWidget {
   const AddNotesButtomSheet({super.key});
@@ -8,12 +13,16 @@ class AddNotesButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding:  EdgeInsets.symmetric(vertical: 32,horizontal: 16),
-      child:  Column(
-        children: [
-       CustomTextField(hint: "Title",maxlines: 1,),
-       SizedBox(height: 16,),
-        CustomTextField(hint: "Content",maxlines: 6,)
-        ],
+      child:  SingleChildScrollView(
+        child: Column(
+          children: [
+         CustomTextField(hint: "Title",maxlines: 1,),
+         SizedBox(height: 16,),
+          CustomTextField(hint: "Content",maxlines: 6,),
+         SizedBox(height: 50,),
+          CustomButton()
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/customNoteItem.dart
+++ lib/widgets/customNoteItem.dart
@@ -1,46 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-            padding: const EdgeInsets.only(top: 16,left: 16),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(24),
-              color: Colors.orange,
-              
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-              ListTile(
-
-                title:const Text("Flutter tips",style: TextStyle(color: Colors.black,
-                fontSize: 26
-                ),),
-                subtitle: Padding(
-                  padding: const EdgeInsets.only(top: 16,bottom: 16,right: 24),
-                  child: Text("build your career with asmaa mousa",style: TextStyle(color: Colors.black.withOpacity(.6),
-                   fontSize:18
-                  ),
-                                 
-                  ),
-                ),
-                trailing: IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
-                )),
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+              padding: const EdgeInsets.only(top: 16,left: 16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24),
+                color: Colors.orange,
+                
               ),
-              Padding(
-                padding: const EdgeInsets.all(24),
-                child: Text("may21,2024",style: TextStyle(color: Colors.black.withOpacity(.6),
-                            
-                )),
-              )
-            ],),
-          );
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                ListTile(
+      
+                  title:const Text("Flutter tips",style: TextStyle(color: Colors.black,
+                  fontSize: 26
+                  ),),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 16,bottom: 16,right: 24),
+                    child: Text("build your career with asmaa mousa",style: TextStyle(color: Colors.black.withOpacity(.6),
+                     fontSize:18
+                    ),
+                                   
+                    ),
+                  ),
+                  trailing: IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                  )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Text("may21,2024",style: TextStyle(color: Colors.black.withOpacity(.6),
+                              
+                  )),
+                )
+              ],),
+            ),
+    );
   }
 }
