3ca8b66f6be5c637e1c02c8bd791536139761f3e
asmaa mousa <asmaamousa978@gmail.com>
2024-11-13 21:33:50 +0200

    add hive to ofline project

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String titile;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.titile,
+     required this.subtitle,
+      required this.date, 
+      required this.color});
+}
